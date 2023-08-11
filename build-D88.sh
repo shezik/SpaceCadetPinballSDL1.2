e2318de54d13aa78d9bd6a625e58a37373163a37
Kerry Shen <47771243+shezik@users.noreply.github.com>
2023-08-11 15:36:53 +0800

    Update build scripts

--- build-D88.sh
+++ build-D88.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
+case $1 in
+clean)
+    rm CMakeCache.txt Makefile cmake_install.cmake
+    rm -r CMakeFiles
+    exit 0;;
+debug|Debug)
+    type="Debug";;
+release|Release|*)
+    type="Release";;
+esac
+
 gcc_path=$(which mipsel-linux-gcc)
-export SDLDIR="${gcc_path}/../../mipsel-buildroot-linux-uclibc/sysroot/usr/lib"  
-# echo ${SDLDIR}
-cmake -D CMAKE_C_COMPILER=mipsel-linux-gcc -D CMAKE_CXX_COMPILER=mipsel-linux-g++ -D CMAKE_C_COMPILER_LAUNCHER="" -D CMAKE_CXX_COMPILER_LAUNCHER="" -D CMAKE_C_LINKER_LAUNCHER="" -D CMAKE_CXX_LINKER_LAUNCHER="" -D SDL2DIR=${SDLDIR} -D CMAKE_BUILD_TYPE=Debug .
+export SDL2DIR="${gcc_path}/../../mipsel-buildroot-linux-uclibc/sysroot/usr/lib"  
+cmake -D CMAKE_C_COMPILER=mipsel-linux-gcc -D CMAKE_CXX_COMPILER=mipsel-linux-g++ -D CMAKE_C_COMPILER_LAUNCHER="" -D CMAKE_CXX_COMPILER_LAUNCHER="" -D CMAKE_C_LINKER_LAUNCHER="" -D CMAKE_CXX_LINKER_LAUNCHER="" -D SDL2DIR=${SDL2DIR} -D CMAKE_BUILD_TYPE=${type} .
---
+++ build-native.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+case $1 in
+clean)
+    rm CMakeCache.txt Makefile cmake_install.cmake
+    rm -r CMakeFiles
+    exit 0;;
+debug|Debug)
+    type="Debug";;
+release|Release|*)
+    type="Release";;
+esac
+
+export SDL2DIR="/lib/x86_64-linux-gnu"  
+cmake -D CMAKE_C_COMPILER=gcc -D CMAKE_CXX_COMPILER=g++ -D CMAKE_C_COMPILER_LAUNCHER="" -D CMAKE_CXX_COMPILER_LAUNCHER="" -D CMAKE_C_LINKER_LAUNCHER="" -D CMAKE_CXX_LINKER_LAUNCHER="" -D SDL2DIR=${SDL2DIR} -D CMAKE_BUILD_TYPE=${type} .
