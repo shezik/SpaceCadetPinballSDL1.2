8e1953c207148616ea476bd5f7f37938109b8ea5
Kerry Shen <47771243+shezik@users.noreply.github.com>
2023-08-11 16:42:18 +0800

    Configure vscode

--- build-D88.sh
+++ build-D88.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+cd $(dirname $0)
+
 case $1 in
 clean)
     rm CMakeCache.txt Makefile cmake_install.cmake
@@ -13,3 +15,4 @@ esac
 gcc_path=$(which mipsel-linux-gcc)
 export SDL2DIR="${gcc_path}/../../mipsel-buildroot-linux-uclibc/sysroot/usr/lib"  
 cmake -D CMAKE_C_COMPILER=mipsel-linux-gcc -D CMAKE_CXX_COMPILER=mipsel-linux-g++ -D CMAKE_C_COMPILER_LAUNCHER="" -D CMAKE_CXX_COMPILER_LAUNCHER="" -D CMAKE_C_LINKER_LAUNCHER="" -D CMAKE_CXX_LINKER_LAUNCHER="" -D SDL2DIR=${SDL2DIR} -D CMAKE_BUILD_TYPE=${type} .
+make -j12
--- build-native.sh
+++ build-native.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+cd $(dirname $0)
+
 case $1 in
 clean)
     rm CMakeCache.txt Makefile cmake_install.cmake
@@ -12,3 +14,4 @@ esac
 
 export SDL2DIR="/lib/x86_64-linux-gnu"  
 cmake -D CMAKE_C_COMPILER=gcc -D CMAKE_CXX_COMPILER=g++ -D CMAKE_C_COMPILER_LAUNCHER="" -D CMAKE_CXX_COMPILER_LAUNCHER="" -D CMAKE_C_LINKER_LAUNCHER="" -D CMAKE_CXX_LINKER_LAUNCHER="" -D SDL2DIR=${SDL2DIR} -D CMAKE_BUILD_TYPE=${type} .
+make -j12
